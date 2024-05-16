bf6a36375023a94150cc98859d8b1a0b6dbc922a
Diego Linhares <diegomlinhares@gmail.com>
2024-05-15 23:47:30 -0300

    feat: Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when "notice" then "bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative"
+    when "success" then "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+    when "error" then "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+    when "alert" then "bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative"
+    else "bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative"
+    end
+  end
 end
